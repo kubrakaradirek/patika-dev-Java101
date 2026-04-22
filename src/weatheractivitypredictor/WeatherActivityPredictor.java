542ab55876f703248603bbcabf6033e40a7b78b0
kubrakaradirek <karadirekkubra96@gmail.com>
2026-04-22 14:24:39 +0300

    feat: implement weather-based activity suggestion with alternative if-else structures

---
+++ src/weatheractivitypredictor/WeatherActivityPredictor.java
@@ -0,0 +1,19 @@
+package weatheractivitypredictor;
+
+import java.util.Scanner;
+
+public class WeatherActivityPredictor {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sıcaklık Giriniz : ");
+        heat = input.nextInt();
+
+        if (heat < 5) System.out.println("Kayak yapabilirsiniz.");
+        if (heat >= 5 && heat <= 15) System.out.println("Sinemaya gidebilirsiniz.");
+        if (heat > 15 && heat <= 25) System.out.println("Pikniğe gidebilirsiniz.");
+        if (heat > 25) System.out.println("Yüzmeye gidebilirsiniz.");
+
+    }
+}
