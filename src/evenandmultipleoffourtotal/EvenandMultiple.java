61bafc08af5d3fa37adb082ef92e6abc827794cf
kubrakaradirek <karadirekkubra96@gmail.com>
2026-04-22 14:49:32 +0300

    feat: sum even numbers and multiples of four until an odd number is entered

---
+++ src/evenandmultipleoffourtotal/EvenandMultiple.java
@@ -0,0 +1,23 @@
+package evenandmultipleoffourtotal;
+
+import java.util.Scanner;
+
+public class EvenandMultiple {
+    public static void main(String[] args) {
+        int n;
+        int total = 0;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı Giriniz : ");
+            n = scan.nextInt();
+
+            if (n % 2 == 0 && n % 4 == 0) {
+                total += n;
+            }
+
+        } while (n % 2 == 0);
+
+        System.out.println("Toplam : " + total);
+    }
+}
